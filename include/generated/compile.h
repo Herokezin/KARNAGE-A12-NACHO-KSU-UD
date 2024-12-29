f3390db896bba9d5413bda9911c94a8b07fd3c7d
Herokezin <bladetrinity466@gmail.com>
2024-12-28 22:34:34 -0500

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Dec 28 22:13:43 -05 2024"
+#define LINUX_COMPILE_BY "herokelc"
+#define LINUX_COMPILE_HOST "herokelc-HP-EliteBook-2560p"
+#define LINUX_COMPILER "Android (7449660, based on r416183d) clang version 12.0.7 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee)"
---
+++ security/samsung/dsms/.built-in.a.cmd
@@ -0,0 +1,1 @@
+cmd_security/samsung/dsms/built-in.a :=  rm -f security/samsung/dsms/built-in.a; ./aarch64-linux-android-4.9-llvm/bin/aarch64-linux-android-ar rcSTPD security/samsung/dsms/built-in.a 
