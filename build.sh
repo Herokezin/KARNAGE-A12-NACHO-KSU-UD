bdc79b24c491f17977e8f4dd5985d0cdffff0306
SNOW <bladetrinity466@gmail.com>
2025-01-02 23:33:08 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
 
-make clean && make mrproper
 make ARCH=arm64 herokezin-A12s_defconfig
-make ARCH=arm64 -j"$(nproc --all)"
+make ARCH=arm64 -j64
+
+
